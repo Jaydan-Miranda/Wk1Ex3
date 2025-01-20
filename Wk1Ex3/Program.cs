cba76d80be52d8a9ee494d32501ca2c082484d90
Jaydan-Miranda <jmmiranda@amarillocollege.com>
2025-01-19 18:05:47 -0600

    final

--- Wk1Ex3/Program.cs
+++ Wk1Ex3/Program.cs
@@ -16,19 +16,19 @@ namespace Wk1Ex3
             // print out the prompt to ask for the principal amount of the loan
             Console.WriteLine("What is principal amount of the loan in dollars? ");
 
-            // declare an int variable using a string to hold the user input for the prinicpal amount
+            // declare an double variable using a string to hold the user input for the prinicpal amount
             double principal = Convert.ToDouble(Console.ReadLine()); // Note this code uses the "Convert.ToDouble" method to convert the string input to a double
 
             // print out the prompt to ask for the principal amount of the loan
             Console.WriteLine("What is the interest rate (input 0.05 for 5%)? ");
 
-            // declare an int variable using a string to hold the user input for the rate
+            // declare an double variable using a string to hold the user input for the rate
             double rate = Convert.ToDouble(Console.ReadLine()); // Note this code uses the "Convert.ToDouble" method to convert the string input to a double
 
             // print out the prompt to ask for the principal amount of the loan
             Console.WriteLine("What is period of loan in years? ");
 
-            // declare an int variable using a string to hold the user input for the prinicpal amount
+            // declare an double variable using a string to hold the user input for the prinicpal amount
             double period = Convert.ToDouble(Console.ReadLine()); // Note this code uses the "Convert.ToDouble" method to convert the string input to a double
 
             // calculates the interest using double data type to properly store the rate decimal variable and calculate the interest
